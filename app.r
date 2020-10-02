1539e98981270bd6d7aa674036024a0b88529b00
Nicholas Lotito <nicholas.lotito@yale.edu>
2020-10-02 11:43:11 -0400

    Working data app
    Update source data, user interface, and backend to generate custom data

--- app.r
+++ app.r
@@ -1,44 +1,92 @@
 # TAC App
 
 library(shiny)
+library(shinyjs)
 library(dplyr)
 library(haven)
 # library(tidyr)
 # library(ggplot2)
 
-load("source.Rdata")
+source("merge.r")
+
+choices_unit <- list("Group-year" = "group", "Dyad-year" = "dyad")
+choices_crit <- list("Political, economic, religious, or social goal" = 1,
+                     "Intention to coerce, intimidate or publicize to larger audience(s)" = 2, 
+                     "Outside international humanitarian law" = 3)
+choices_targtype <- list("Business" = 1,
+                         "Government (general)" = 2,
+                         "Police" = 3,
+                         "Military" = 4,
+                         "Abortion related" = 5,
+                         "Airports & aircraft" = 6,
+                         "Government (diplomatic)" = 7,
+                         "Educational institution" = 8,
+                         "Food or water supply" = 9,
+                         "Journalists & media" = 10,
+                         "Maritime" = 11,
+                         "NGO" = 12,
+                         "Other" = 13,
+                         "Private citizens & property" = 14,
+                         "Religious figures/institutions" = 15,
+                         "Telecommunication" = 16,
+                         "Terrorists/non-state militias" = 17,
+                         "Tourists" = 18,
+                         "Transportation (non-aviation)" = 19,
+                         "Unknown" = 20,
+                         "Utilities" = 21,
+                         "Violent political parties" = 22)
+
+choices_attacktype <- list("Assassination" = 1,
+                           "Armed assault" = 2,
+                           "Bombing/explosion" = 3,
+                           "Hijacking" = 4,
+                           "Hostage taking (barricade)" = 5,
+                           "Hostage taking (kidnapping)" = 6,
+                           "Facility/infrastructure" = 7,
+                           "Unarmed assault" = 8,
+                           "Unknown" = 9)
 
 ui <- pageWithSidebar(
-  headerPanel("TAC Demo App"),
-  # headerPanel("Download Terrorism in Armed Conflict (TAC) Data"),
+  # headerPanel("TAC Data App"),
+  headerPanel("Generate TAC Count Data"),
   sidebarPanel(
-    checkboxGroupInput('match', 'UCDP-GTD Matching', choices_gname_match),
-    checkboxGroupInput("crit", "Terrorism Definition:", choices_crit,
-                       selected = c(1,2,3)),
-    checkboxGroupInput("attacktype", "Attack Types:", choices_attacktype, 
-                       inline = TRUE, selected = c(2,3,4,5,7)),
-    checkboxGroupInput("attacktype", "Target Types:", choices_targtype, inline = TRUE,
-                       selected = c(1, 6, 8, 9, 11, 13, 14, 15, 16, 18, 19, 21))
-  ),
+    downloadButton("downloadCSV", "Download as CSV"),
+    useShinyjs(), # Set up shinyjs
+    br(), br(),
+    actionButton("reset", "Reset to Defaults")),
   mainPanel(
-    tableOutput("data"),
-    downloadButton("downloadCSV", "Download as CSV")
+    useShinyjs(), # Set up shinyjs
+    div(id = "form",
+        radioButtons("unit", 'Unit of observation', choices_unit),
+        checkboxGroupInput("crit", "Required Criteria", choices_crit,
+                           selected = c(1,2,3)),
+        checkboxGroupInput("attacktype", "Included Attack Types", choices_attacktype,
+                           inline = TRUE, selected = c(2,3,4,5,7)),
+        checkboxGroupInput("targtype", "Included Target Types", choices_targtype, inline = FALSE,
+                           selected = c(1, 6, 8, 9, 11, 13, 14, 15, 16, 18, 19, 21))
+    )
   )
 )
 
 server <- function(input, output) {
   
-  output$data <- renderTable({
-      head(link)
-  }, rownames = TRUE)
+  # output$data <- renderTable({
+  #     head(link)TRUE %in% TRUE
+  # }, rownames = TRUE)
 
   output$downloadCSV <- downloadHandler(
     content = function(file) {
-      write.csv(link, file)
+      showNotification("Generating data")
+      write.csv(tac.generate(input$unit, input$crit, input$attacktype, input$targtype), file)
     },
-    filename = "TAC.csv",
+    filename = "TAC_custom.csv",
     contentType = "text/csv"
   )
+  
+  observeEvent(input$reset, {
+    reset("form")
+  })
+  
 }
 
 shinyApp(ui, server)
--- merge.r
+++ merge.r
@@ -1,18 +1,14 @@
-# TAC Merge
-# Nick Lotito, v.2019-09
-library(dplyr)
-library(tidyr)
-library(rlang)
+# Generate Custom TAC Count Data
+# Nick Lotito, v.2020-09
 
-# options(crayon.enabled = FALSE) #temp fix to disable colors in dplyr output
+require(dplyr)
+require(tidyr)
 
-setwd("~/GitHub/TAC")
 load("source.Rdata")
 
-# attack.list <- c(2:6,9)
-# target.list <- c(1,6,8,9,11,13:16,18:21)
-
-as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
+######################################################
+# Define UCDP-GTD match levels
+######################################################
 
 matches <- tribble(
     ~gname_match, ~match,
@@ -27,41 +23,75 @@ matches <- tribble(
     13,"f"
 )
 
-attack.list <- c(2:6,9)
-target.list <- c(1,6,8,9,11,13:16,18:21)
+######################################################
+# Define TAC generation function
+######################################################
 
-attack.more.list <- c(2,3)
-subtarg.more.list <- c(2, 7, 8, 11, 42, 44, 49:52, 57, 60, 65:67, 69:81, 86, 87, 95:105)
+tac.generate <- function(unit = "group",
+                         crit = c(1:3), 
+                         attacktype = c(2:6,9),
+                         targtype = c(1,6,8,9,11,13:16,18:21)){
+    
+    crit <- as.integer(crit)
+    my.crit1 <- ifelse(1 %in% crit, TRUE, FALSE)
+    my.crit2 <- ifelse(2 %in% crit, TRUE, FALSE)
+    my.crit3 <- ifelse(3 %in% crit, TRUE, FALSE)
 
+    attack.list <- as.integer(attacktype)
+    target.list <- as.integer(targtype)
 
-df <- left_join(link, gtd) %>%
-	mutate(crit = ifelse(crit1 == TRUE & crit2 == TRUE & crit3 == TRUE, T, F),
-		attack = ifelse(attacktype1 %in% attack.list | attacktype2 %in% attack.list | attacktype3 %in% attack.list, T, F),
-		target = ifelse(targtype1 %in% target.list, T, F),
-		attack.more = ifelse(attacktype1 %in% attack.more.list | attacktype2 %in% attack.more.list | attacktype3 %in% attack.more.list, T, F),
-		target.more = ifelse(targsubtype1 %in% subtarg.more.list, T, F)) %>%
-	select(dyadid, year, gname_match, fatal:target.more) %>%
-	mutate(t = ifelse(crit == T & attack == T & target == T, T, F),
-	       f = ifelse(t == T & fatal == T, T, F),
-	       m = ifelse(t == T & mass == T, T, F),
-	       tm = ifelse(crit == T & attack.more == T & target.more == T, T, F),
-	       fm = ifelse(tm == T & fatal == T, T, F),
-	       mm = ifelse(tm == T & mass == T, T, F),
-	       gname_match = as.numeric.factor(gname_match)) %>%
-    left_join(matches) %>%
-    filter(!is.na(match)) %>%
-    arrange(dyadid, year, match, t) %>%
-    group_by(dyadid, year, match) %>%
-    summarize_at(vars(t:mm), sum) %>%
-    ungroup() %>% 
-    complete(nesting(dyadid,year),match,fill = list(t = 0, f = 0, m = 0, tm = 0, fm = 0, mm = 0)) %>% 
-    group_by(dyadid, year) %>% 
-    mutate_at(vars(t:mm), cumsum) %>% 
-    gather(key, value, -dyadid, -year, -match) %>% 
-    mutate(variable = paste0(key, "_", match)) %>% 
-    select(-match, -key) %>% 
-    spread(variable, value) %>% 
-    ungroup() %>% 
-    mutate_all(as.integer)
+    as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
 
-df
+    if (unit == "group") {
+        df <- left_join(df.source, gtd, by = "eventid") %>% 
+            rename(uid = sidebid)
+    } else if (unit == "dyad") {
+        df <- left_join(df.source, gtd, by = "eventid") %>%
+            rename(uid = dyadid)
+    } else {
+        stop("'unit' must be 'group' or 'dyad'")
+    }
+
+    # generate counts
+    df <- df %>%
+        mutate(crit = ifelse((crit1 == T | my.crit1 == F) & 
+                             (crit2 == T | my.crit2 == F) & 
+                             (crit3 == T | my.crit3 == F), T, F),
+            attack = ifelse(attacktype1 %in% attack.list | attacktype2 %in% attack.list | attacktype3 %in% attack.list, T, F),
+            target = ifelse(targtype1 %in% target.list, T, F)) %>%
+        select(uid, year, gname_match, fatal:target) %>%
+        mutate(t = ifelse(crit == T & attack == T & target == T, T, F),
+               f = ifelse(t == T & fatal == T, T, F),
+               m = ifelse(t == T & mass == T, T, F),
+               gname_match = as.numeric.factor(gname_match)) %>%
+        left_join(matches, by = "gname_match") %>%
+        filter(!is.na(match)) %>%
+        arrange(uid, year, match, t) %>%
+        group_by(uid, year, match) %>%
+        summarize_at(vars(t:m), sum) %>%
+        ungroup() %>% 
+        complete(nesting(uid,year),match,fill = list(t = 0, f = 0, m = 0)) %>%
+        group_by(uid, year) %>% 
+        mutate_at(vars(t:m), cumsum) %>% 
+        gather(key, value, -uid, -year, -match) %>% 
+        mutate(variable = paste0(key, "_", match)) %>% 
+        select(-match, -key) %>% 
+        spread(variable, value) %>% 
+        ungroup() %>% 
+        mutate_all(as.integer)
+
+    # expand to group/dyad date range
+    if (unit == "group") {
+        df <- left_join(date.range.group, df, by = c("uid", "year"))
+        df <- bind_rows(filter(df, year == 1993),
+                 mutate_all(filter(df, year != 1993), ~replace_na(., 0))) %>% 
+              arrange(uid, year) %>% 
+              rename(sidebid = uid)
+    } else if (unit == "dyad") {
+        df <- left_join(date.range.dyad, df, by = c("uid", "year"))
+        df <- bind_rows(filter(df, year == 1993),
+                 mutate_all(filter(df, year != 1993), ~replace_na(., 0))) %>% 
+              arrange(uid, year) %>% 
+              rename(dyadid = uid)
+    }
+}
